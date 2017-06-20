299e3a29691193c735282e7a68caba3ce2688973
Heather Thacker <hhthacker@gmail.com>
2017-06-19 20:45:17 -0500

    appcon

--- public/javascripts/AppConstants.js
+++
@@ -1,8 +0,0 @@
-app.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyCulIeEFJpnBYrVCZ_YHiPSkzv_BKIHjNI",
-    authDomain: "bread-d6bfb.firebaseapp.com",
-    databaseURL: "https://bread-d6bfb.firebaseio.com",
-    projectId: "bread-d6bfb",
-    storageBucket: "bread-d6bfb.appspot.com",
-    messagingSenderId: "19131201198"
-});
