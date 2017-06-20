b0ac2b64e4519181426289d7af136fd0775aca1a
Heather Thacker <hhthacker@gmail.com>
2017-06-19 19:20:27 -0500

    Delete AppConstants.js

--- public/javascripts/AppConstants.js
+++
@@ -1,8 +0,0 @@
-app.constant("FIREBASE_CONFIG", {
-    apiKey: "AIzaSyCzJDZ4_RHGIySohU-UOaG1vAFX574IG_g",
-    authDomain: "crustandcrumb-99bb4.firebaseapp.com",
-    databaseURL: "https://crustandcrumb-99bb4.firebaseio.com",
-    projectId: "crustandcrumb-99bb4",
-    storageBucket: "crustandcrumb-99bb4.appspot.com",
-    messagingSenderId: "321163254888"
-});
